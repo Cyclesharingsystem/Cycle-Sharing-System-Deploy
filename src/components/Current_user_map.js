6b34fa3c691ec2988b8d6c9f9b2409a49ab07fac
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-06-26 18:48:02 +0530

    user map image icon add

--- src/components/Current_user_map.js
+++ src/components/Current_user_map.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Map, GoogleApiWrapper, Marker } from '@peacechen/google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from "@peacechen/google-maps-react";
+import bicycleIcon from "../images/bicycle.png";
 import axios from "axios";
 
 const Current_user_map = ({ google, rideId }) => {
@@ -7,7 +8,9 @@ const Current_user_map = ({ google, rideId }) => {
 
   const fetchCurrentLocation = async () => {
     try {
-      const response = await axios.get(`https://backend-host-9thd.onrender.com/api/v1/ride/${rideId}/currentLocation`);
+      const response = await axios.get(
+        `https://backend-host-9thd.onrender.com/api/v1/ride/${rideId}/currentLocation`
+      );
       setRideLocation(response.data);
     } catch (error) {
       console.error("Error fetching current location:", error);
@@ -27,18 +30,25 @@ const Current_user_map = ({ google, rideId }) => {
         style={{ width: "35.7%", height: "55%" }}
         zoom={15}
         initialCenter={{
-          lat: rideLocation ? rideLocation.latitude : 6.047170, // Fallback value
-          lng: rideLocation ? rideLocation.longitude : 80.210091 // Fallback value
+          lat: rideLocation ? rideLocation.latitude : 6.04717, // Fallback value
+          lng: rideLocation ? rideLocation.longitude : 80.210091, // Fallback value
         }}
         center={{
-          lat: rideLocation ? rideLocation.latitude : 6.047170, // Update center dynamically
-          lng: rideLocation ? rideLocation.longitude : 80.210091 // Update center dynamically
+          lat: rideLocation ? rideLocation.latitude : 6.04717, // Update center dynamically
+          lng: rideLocation ? rideLocation.longitude : 80.210091, // Update center dynamically
         }}
       >
         {rideLocation && (
           <Marker
-            position={{ lat: rideLocation.latitude, lng: rideLocation.longitude }}
+            position={{
+              lat: rideLocation.latitude,
+              lng: rideLocation.longitude,
+            }}
             title={`Current Location: Latitude: ${rideLocation.latitude}, Longitude: ${rideLocation.longitude}, Timestamp: ${rideLocation.timestamp}`}
+            icon={{
+              url: bicycleIcon,
+              scaledSize: new window.google.maps.Size(40, 40), // Set the size of the image
+            }}
           />
         )}
       </Map>
